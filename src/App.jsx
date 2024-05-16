cd088805361561cc15c0c13aa32782dd9cb0a980
Vatsalmadhur <bhartimadhur@gmail.com>
2024-05-16 11:58:43 +0530

    ramdom commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import { Navbar } from './components/Navbar'
 import Model from './components/Model'
+import * as Sentry from '@sentry/react'
+import Features from './components/Features'
 function App() {
-
   return (
     <>
       <Navbar/>
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
     </>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,20 @@
+import gsap from "gsap"
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger);
+export const animateWthGsap=(target,animatonProps,scrollProps)=>{
+    gsap.to(target,
+        {
+            ...animatonProps,
+            scrollTrigger:{
+                trigger:target,
+                toggleActions:'restart reverse restart reverse',
+                start:'top(85%)',
+                ...scrollProps
+            }
+        }
+    )
+
+}
 export const animateWithGsapTimeline =(timeline,rotationRef,rotationState,firstTarget,secondTarget,animationProps)=>{
     timeline.to(firstTarget,
         {
